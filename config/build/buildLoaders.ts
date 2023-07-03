4296d6a8066f8199900143350d68d3474f7a39bf
Artem <artekalinin@mail.ru>
2023-07-03 23:07:39 +0300

    master

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -27,8 +27,22 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         exclude: /node_modules/,
     }
 
+    const svgLoader =   {
+        test: /\.svg$/,
+        use:['@svgr/webpack'],
+    }
+    const fileLoader =  {
+        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      }
     return [
         typescriptLoader,
         cssLoader,
+        svgLoader,
+        fileLoader
     ]
 }
---
+++ src/app/App.tsx
@@ -0,0 +1,33 @@
+import './styles/index.scss';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from './provieders/ThemeProvider';
+import { AppRouter } from './provieders/router';
+import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
+
+
+
+export enum Theme{
+    NORMAL='normal',
+    DARK='dark'
+}
+
+const App = () => {
+    const {theme} = useTheme()
+    
+    return (
+        <div className={classNames('app',{},[theme])}>
+            
+            <Navbar/>
+            <div className='content-page'>
+                <Sidebar/>
+                <AppRouter/>
+            </div>
+           
+           
+        </div>
+    );
+};
+
+export default App;
+
